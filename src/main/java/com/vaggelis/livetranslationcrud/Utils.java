e1ce91cf2a9436b97042c3e230408d4737cccd56
VaggV <vag_vasiliou@hotmail.com>
2022-05-14 18:47:33 +0300

    -

--- src/main/java/com/vaggelis/livetranslationcrud/Utils.java
+++ src/main/java/com/vaggelis/livetranslationcrud/Utils.java
@@ -4,7 +4,6 @@ import org.apache.commons.text.similarity.LevenshteinDistance;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class Utils {
     public static double similarity(String s1, String s2) {
